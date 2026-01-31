23761dec3b42edab0c1e265ac82c70610440ab15
Hiren Gajjar <hirengajjar@Hirens-MacBook-Pro.local>
2026-01-31 00:58:44 -0800

    new number pattern

--- patterns/code.cpp
+++ patterns/code.cpp
@@ -42,16 +42,33 @@ int main()
     ABCD
     ABCD
     */
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char ch = 'A';
+
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << ch;
+    //         ch++;
+    //     }
+
+    //     cout << endl;
+    // }
+
+    /*
+    123
+    456
+    789
+    */
+    int k = 1;
     for (int i = 0; i < n; i++)
     {
-        char ch = 'A';
 
         for (int j = 0; j < n; j++)
         {
-            cout << ch;
-            ch++;
+            cout << k << " ";
+            k++;
         }
-
         cout << endl;
     }
     return 0;
