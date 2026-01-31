a46200a63ff7d830ddf8718570cd7c4ce8ce704b
Hiren Gajjar <hirengajjar@Hirens-MacBook-Pro.local>
2026-01-31 00:49:28 -0800

    alphabet patterns

--- patterns/code.cpp
+++ patterns/code.cpp
@@ -2,21 +2,56 @@
 using namespace std;
 int main()
 {
+    int n;
+    cout << "Enter the value of n: " << endl;
+    cin >> n;
     /*
     ****
     ****
     ****
     ****
     */
-    int n;
-    cout << "Enter the value of n: " << endl;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    1234
+    1234
+    1234
+    1234
+     */
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    ABCD
+    ABCD
+    ABCD
+    ABCD
+    */
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        char ch = 'A';
+
+        for (int j = 0; j < n; j++)
         {
-            cout << "*";
+            cout << ch;
+            ch++;
         }
+
         cout << endl;
     }
     return 0;
