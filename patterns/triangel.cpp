df5134c8d17c7dcffbc9c8ad354ac51641f10421
Hiren Gajjar <hirengajjar@Hirens-MacBook-Pro.local>
2026-01-31 01:30:26 -0800

    number triangel pattern

--- patterns/triangel.cpp
+++ patterns/triangel.cpp
@@ -73,14 +73,40 @@ int main()
     //     ch++;
     // }
 
-    char ch = 'A';
+    // char ch = 'A';
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout << ch;
+    //     }
+    //     ch++;
+    //     cout << endl;
+    // }
+
+    /*
+    1
+    12
+    123
+    1234
+    12345
+    */
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j <= i; j++)
         {
-            cout << ch;
+            cout << j + 1;
         }
-        ch++;
         cout << endl;
     }
     return 0;
