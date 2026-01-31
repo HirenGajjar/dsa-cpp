220fc7abbbef53dddec98344806d4b8c4832f3e1
Hiren Gajjar <hirengajjar@Hirens-MacBook-Pro.local>
2026-01-31 01:46:26 -0800

    floyd's pattern

--- patterns/triangel.cpp
+++ patterns/triangel.cpp
@@ -101,13 +101,39 @@ int main()
     //     cout << endl;
     // }
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j <= i; j++)
-        {
-            cout << j + 1;
-        }
-        cout << endl;
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout << j + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    1
+    21
+    321
+    4321
+    54321
+    */
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = i + 1; j > 1; j--)
+    //     {
+    //         cout << j - 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j > 0; j--)
+    //     {
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
     return 0;
 }
