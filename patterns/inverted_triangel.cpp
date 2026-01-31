be0d039917a9abe07ad16851a4d7711b9e88bf56
Hiren Gajjar <hirengajjar@Hirens-MacBook-Pro.local>
2026-01-31 02:34:56 -0800

    start pattern basic way

---
+++ patterns/inverted_triangel.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+    1111
+     222
+      33
+       4
+    */
+    int n;
+    cout << "Enter the value of n : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for ()
+        {
+        }
+    }
+    return 0;
+}
