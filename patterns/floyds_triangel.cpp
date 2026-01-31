83e5ab907b6f904bc74f8e6aa6e44864dc1c2fd7
Hiren Gajjar <hirengajjar@Hirens-MacBook-Pro.local>
2026-01-31 02:09:17 -0800

    floyd's triangel with char

--- patterns/floyds_triangel.cpp
+++ patterns/floyds_triangel.cpp
@@ -12,12 +12,91 @@ int main()
     int n, k = 1;
     cout << "Enter the number : ";
     cin >> n;
-    for (int i = 1; i <= n; i++)
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << k;
+    //         k++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout << k;
+    //         k++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j > 0; j--)
+    //     {
+    //         cout << k << " ";
+    //         k++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = i; j > 0; j--)
+    //     {
+    //         cout << k << " ";
+    //         k++;
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    A
+    BC
+    DEF
+    GHIJ
+    KLMNO
+    PQRSTU
+     */
+    char ch = 'A';
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = i; j > 0; j--)
+    //     {
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = i + 1; j > 0; j--)
         {
-            cout << k;
-            k++;
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
