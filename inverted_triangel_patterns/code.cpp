f856edb8cd2cc6e56dd888c4e46588f8001d5654
Hiren Gajjar <hirengajjar@Hirens-MacBook-Pro.local>
2026-02-01 04:52:50 -0800

    same pattern with numbers and alphabets

--- inverted_triangel_patterns/code.cpp
+++ inverted_triangel_patterns/code.cpp
@@ -12,7 +12,7 @@ int main()
     //     {
     //         cout << " ";
     //     }
-    //     for (int j = 1; j <= n - i; j++)
+    //     for (int j = 1; j <= n +1- i; j++)
     //     {
     //         cout << "*";
     //     }
@@ -32,6 +32,72 @@ int main()
     //     cout << endl;
     // }
 
-    
+    /*
+    1111
+     222
+      33
+       4
+    */
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int j = 1; j <= n + - i; j++)
+    //     {
+    //         cout << i;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int j = 0; j <= n - 1 - i; j++)
+    //     {
+    //         cout << i + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    AAAA
+     BBB
+      CC
+       D
+    */
+    char ch = 'A';
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int j = 1; j <= n + 1 - i; j++)
+    //     {
+    //         cout << ch;
+    //     }
+    //     cout << endl;
+    //     ch++;
+    // }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j <= n - 1 - i; j++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+        ch++;
+    }
     return 0;
 }
