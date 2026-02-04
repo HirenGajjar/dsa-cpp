f1b50b64c762f75900d32222f8a9a292b6e51e7a
Hiren Gajjar <hirengajjar@Hirens-MacBook-Pro.local>
2026-02-04 00:45:00 -0800

    add functions for prime numbers and fibonacci

--- functions/code.cpp
+++ functions/code.cpp
@@ -95,24 +95,86 @@ int binomialCoefficient(int n, int r)
     return (factorial(n) / (factorial(r) * factorial(n - r)));
 }
 
+int isPrime(int num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int allPrimeBetweenOneToN(int num)
+{
+    for (int i = 2; i <= num; i++)
+    {
+        if (isPrime(i) == true)
+        {
+            cout << i << ",";
+        }
+    }
+    return 0;
+}
+
+int fibonacci(int which_number)
+{
+    int term_one = 0, term_two = 1, next_term;
+    if (which_number == 1)
+    {
+        return term_one;
+    }
+    else if (which_number == 2)
+    {
+        return term_two;
+    }
+    else
+    {
+        for (int i = 3; i <= which_number; i++)
+        {
+            cout << term_one << ", ";
+            next_term = term_one + term_two;
+            term_one = term_two;
+            term_two = next_term;
+        }
+        cout << endl;
+    }
+    return 0;
+}
 int main()
 {
-    cout << sum(4, 5.4) << endl;
-    cout << mul(12, 5) << endl;
-    cout << divi(60, 12) << endl;
-    cout << subt(4, 5.4) << endl;
-    cout << minOfTwo(15.5, 15.49) << endl;
-    cout << sumOfN(10) << endl;
-    cout << factorial(5) << endl;
-    cout << noActionAfterReturn();
-    cout << sumOnCallByReference(12, 12) << endl;
-    int refOfa = 4, refOfb = 6;
-    cout << sumOnCallByValue(refOfa, refOfb) << endl;
-    int num = 1234131231;
-    cout << "Sum of digits of " << num << " is " << sumOfTheDigitsOfTheNumber(num) << endl;
+    // cout << sum(4, 5.4) << endl;
+    // cout << mul(12, 5) << endl;
+    // cout << divi(60, 12) << endl;
+    // cout << subt(4, 5.4) << endl;
+    // cout << minOfTwo(15.5, 15.49) << endl;
+    // cout << sumOfN(10) << endl;
+    // cout << factorial(5) << endl;
+    // cout << noActionAfterReturn();
+    // cout << sumOnCallByReference(12, 12) << endl;
+    // int refOfa = 4, refOfb = 6;
+    // cout << sumOnCallByValue(refOfa, refOfb) << endl;
+    // int num = 1234131231;
+    // cout << "Sum of digits of " << num << " is " << sumOfTheDigitsOfTheNumber(num) << endl;
 
-    cout << binomialCoefficient(8, 2) << endl;
+    // cout << binomialCoefficient(8, 2) << endl;
 
-    cout << "Is 12 a prime Number ? " << isPrime(12) << endl;
+    // cout << "Is 12 a prime Number ? " << isPrime(12) << endl;
+
+    // int number;
+    // cout << "Enter the value of number : ";
+    // cin >> number;
+    // allPrimeBetweenOneToN(number);
+
+    int which_number;
+    cout << "Enter the number : ";
+    cin >> which_number;
+    fibonacci(which_number);
     return 0;
 }
