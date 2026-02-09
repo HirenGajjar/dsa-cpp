d44640f8e53785bcaa918d33ee5b873acfbd932d
HirenGajjar <gajjarhiren111@gmail.com>
2026-02-09 01:06:51 -0800

    largest from the array

---
+++ arrays/largest_in_array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int largest = INT_MIN;
+int main()
+{
+    int array[] = {3, 4, 5, 3, 23, 234, 34, -34, 3, 42, 34, 23, 42, 34, 23412, 2, 34, 234};
+    for (int i = 0; i <= (sizeof(array) / sizeof(int)) - 1; i++)
+    {
+        if (largest < array[i])
+        {
+            largest = array[i];
+        }
+    }
+    cout << "Largest value from the array is : " << largest << endl;
+    return 0;
+}
