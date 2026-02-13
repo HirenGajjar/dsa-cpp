948b5c2906402987b7908d3c2ba0532256152c8d
HirenGajjar <gajjarhiren111@gmail.com>
2026-02-12 18:19:52 -0800

    leetcode 169 brute force approach

--- 11_majority_sum/167_two_sum.cpp
+++ 11_majority_sum/167_two_sum.cpp
@@ -17,11 +17,38 @@ using namespace std;
 //     return {};
 // }
 
+vector<int> pair_sum_optimal(vector<int> vec, int target)
+{
+
+    vector<int> answer;
+    int start = 0, end = vec.size() - 1;
+    while (start < end)
+    {
+        if (vec[start] + vec[end] > target)
+        {
+            end--;
+        }
+        else if (vec[start] + vec[end] < target)
+        {
+            start++;
+        }
+        else if (vec[start] + vec[end] == target)
+        {
+            answer.push_back(start);
+            answer.push_back(end);
+            break;
+        }
+    }
+    return answer;
+}
 int main()
 {
     vector<int> vec = {2, 7, 11, 15};
     int target = 9;
-    vector<int> answer = pair_sum_brute_force(vec, target);
+    // vector<int> answer = pair_sum_brute_force(vec, target);
+    // cout << answer[0] << "," << answer[1];
+
+    vector<int> answer = pair_sum_optimal(vec, target);
     cout << answer[0] << "," << answer[1];
     cout << endl;
     return 0;
---
+++ 11_majority_sum/169_majority_element.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int majority_element_brute_force(vector<int> vec, int n)
+{
+    for (int value : vec)
+    {
+        int majority_element = 0;
+        for (int item : vec)
+        {
+            if (value == item)
+            {
+                majority_element++;
+            }
+        }
+        if (majority_element > n)
+        {
+            return value;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> vec = {1, 2, 2, 4, 1, 4, 1, 4, 4, 4, 4, 4};
+    int n = floor(vec.size() / 2);
+    int answer = majority_element_brute_force(vec, n);
+    cout << "Majority element : " << answer << endl;
+    cout << endl;
+    return 0;
+}
