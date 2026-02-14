005647fb54b1b1bcf89947d78cee323356e74908
HirenGajjar <gajjarhiren111@gmail.com>
2026-02-14 03:09:16 -0800

    plus one -  slighty better approach

--- 66.cpp
+++ 66.cpp
@@ -1,37 +1,75 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+// vector<int> plus_one_brute_force(vector<int> vec)
+
+// {
+//     vector<int> answer;
+//     int size = vec.size();
+//     int decimal = 0;
+//     int power = 1;
+
+//     for (int i = size - 1; i >= 0; i--)
+//     {
+//         decimal += (power * vec[i]);
+//         power *= 10;
+//     }
+//     decimal++;
+
+//     while (decimal > 0)
+//     {
+//         answer.insert(answer.begin(), decimal % 10);
+//         decimal /= 10;
+//     }
+//     return answer;
+// }
+
 vector<int> plus_one(vector<int> vec)
-
 {
-    vector<int> answer;
-    int size = vec.size();
-    int decimal = 0;
-    int power = 1;
 
-    for (int i = size - 1; i >= 0; i--)
+    if (vec.size() == 1 && vec[vec.size() - 1] == 9)
     {
-        decimal += (power * vec[i]);
-        power *= 10;
+        vec.pop_back();
+        vec.push_back(1);
+        vec.push_back(0);
     }
-    decimal++;
 
-    while (decimal > 0)
+    else
     {
-        answer.insert(answer.begin(), decimal % 10);
-        decimal /= 10;
+        for (int i = vec.size() - 1; i >= 0; i--)
+        {
+
+            if (vec[i] != 9)
+            {
+                vec[i]++;
+                break;
+            }
+            else if (vec[i] == 9)
+            {
+                vec[i] = 0;
+            }
+        }
+        if (vec[0] == 0)
+        {
+            vec[0] = 1;
+            vec.push_back(0);
+        }
     }
-    return answer;
+    return vec;
 }
 int main()
 {
-    vector<int> vec = {1, 9, 9};
+    vector<int> vec = {0};
+    // vector<int> answer = plus_one_brute_force(vec);
+    // for (int i : answer)
+    // {
+    //     cout << i << " ";
+    // }
     vector<int> answer = plus_one(vec);
     for (int i : answer)
     {
         cout << i << " ";
     }
-
     cout << endl;
 
     return 0;
