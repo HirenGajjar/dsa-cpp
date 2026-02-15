f89458567cd25ccc5de3f0e7f98118b082d2dba5
HirenGajjar <gajjarhiren111@gmail.com>
2026-02-15 01:14:43 -0800

    very special case where there is no majority element exist - solution with moores algoritm'

--- 11_majority_sum/169_majority_element.cpp
+++ 11_majority_sum/169_majority_element.cpp
@@ -52,17 +52,38 @@ using namespace std;
 //     return vec[vec.size() / 2];
 // }
 
-int majority_element_moores_vote_algorithm(vector<int> vec)
+// int majority_element_moores_vote_algorithm(vector<int> vec)
+// {
+//     int frequency = 0, answer = 0;
+//     for (int i = 0; i < vec.size(); i++)
+//     {
+
+//         if (frequency == 0)
+//         {
+//             answer = vec[i];
+//         }
+//         if (answer == vec[i])
+//         {
+//             frequency++;
+//         }
+//         else
+//         {
+//             frequency--;
+//         }
+//     }
+//     return answer;
+// }
+
+int moores_algorithm_where_majority_element_does_not_exist(vector<int> vec)
 {
-    int frequency = 0, answer = 0;
+    int frequency = 0, answer = 0, count = 0;
     for (int i = 0; i < vec.size(); i++)
     {
-
         if (frequency == 0)
         {
             answer = vec[i];
         }
-        if (answer == vec[i])
+        if (vec[i] == answer)
         {
             frequency++;
         }
@@ -71,13 +92,29 @@ int majority_element_moores_vote_algorithm(vector<int> vec)
             frequency--;
         }
     }
-    return answer;
+    for (int value : vec)
+    {
+        if (value == answer)
+        {
+            count++;
+        }
+    }
+    if (count > vec.size() / 2)
+    {
+        return answer;
+    }
+    else
+    {
+        return -1;
+    }
 }
 int main()
 {
     vector<int> vec = {1, 3, 1, 1, 4, 1, 1, 5, 1, 1, 6, 2, 2};
 
     vector<int> vec2 = {3, 2, 3};
+
+    vector<int> vec3 = {1, 2, 3, 4};
     int n = vec.size();
     // int answer = majority_element_brute_force(vec, n);
     // cout << "Majority element : " << answer << endl;
@@ -88,7 +125,10 @@ int main()
     // int answer = majority_element_sorted_variation(vec);
     // cout << answer;
 
-    int answer = majority_element_moores_vote_algorithm(vec);
+    // int answer = majority_element_moores_vote_algorithm(vec);
+    // cout << "Answer : " << answer;
+
+    int answer = moores_algorithm_where_majority_element_does_not_exist(vec3);
     cout << "Answer : " << answer;
     cout << endl;
     return 0;
