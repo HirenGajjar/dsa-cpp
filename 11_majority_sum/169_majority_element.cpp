02c46fcbf43e98de0e3fa5865fcb8d3640be0064
HirenGajjar <gajjarhiren111@gmail.com>
2026-02-15 00:36:22 -0800

    majority element with sorted vector and two variations

--- 11_majority_sum/169_majority_element.cpp
+++ 11_majority_sum/169_majority_element.cpp
@@ -21,15 +21,49 @@ using namespace std;
 //     return -1;
 // }
 
-vector<int> majority_element_sorted(vector<int> vec, int n)
-{
-}
+// int majority_element_sorted(vector<int> vec, int n)
+// {
+//     sort(vec.begin(), vec.end());
+//     int frequency = 1;
+//     int answer = vec[0];
+
+//     for (int i = 1; i <= n - 1; i++)
+//     {
+//         if (vec[i] == vec[i - 1])
+//         {
+//             frequency++;
+//         }
+//         else
+//         {
+//             frequency = 1;
+//             answer = vec[i];
+//         }
+//         if (frequency > n / 2)
+//         {
+//             return answer;
+//         }
+//     }
+//     return answer;
+// }
+
+// int majority_element_sorted_variation(vector<int> vec)
+// {
+//     sort(vec.begin(), vec.end());
+//     return vec[vec.size() / 2];
+// }
+
 int main()
 {
-    vector<int> vec = {1, 2, 2, 4, 1, 4, 1, 4, 4, 4, 4, 4};
-    int n = floor(vec.size() / 2);
+    vector<int> vec = {1, 3, 1, 1, 4, 1, 1, 5, 1, 1, 6, 2, 2};
+    int n = vec.size();
     // int answer = majority_element_brute_force(vec, n);
     // cout << "Majority element : " << answer << endl;
+
+    // int answer = majority_element_sorted(vec, n);
+    // cout << answer;
+
+    // int answer = majority_element_sorted_variation(vec);
+    // cout << answer;
     cout << endl;
     return 0;
 }
