5b7d3a7d4ff8e16c32e22082d52671077fea86fe
HirenGajjar <gajjarhiren111@gmail.com>
2026-02-15 00:51:26 -0800

    correct code for moores algorithm

--- 11_majority_sum/169_majority_element.cpp
+++ 11_majority_sum/169_majority_element.cpp
@@ -52,9 +52,32 @@ using namespace std;
 //     return vec[vec.size() / 2];
 // }
 
+int majority_element_moores_vote_algorithm(vector<int> vec)
+{
+    int frequency = 0, answer = 0;
+    for (int i = 0; i < vec.size(); i++)
+    {
+
+        if (frequency == 0)
+        {
+            answer = vec[i];
+        }
+        if (answer == vec[i])
+        {
+            frequency++;
+        }
+        else
+        {
+            frequency--;
+        }
+    }
+    return answer;
+}
 int main()
 {
     vector<int> vec = {1, 3, 1, 1, 4, 1, 1, 5, 1, 1, 6, 2, 2};
+
+    vector<int> vec2 = {3, 2, 3};
     int n = vec.size();
     // int answer = majority_element_brute_force(vec, n);
     // cout << "Majority element : " << answer << endl;
@@ -64,6 +87,9 @@ int main()
 
     // int answer = majority_element_sorted_variation(vec);
     // cout << answer;
+
+    int answer = majority_element_moores_vote_algorithm(vec);
+    cout << "Answer : " << answer;
     cout << endl;
     return 0;
 }
