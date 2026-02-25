215deaf46acb6bb679f784335beadd8b4c9d70f1
HirenGajjar <gajjarhiren111@gmail.com>
2026-02-25 12:45:49 -0800

    gcd/hcf best approach

--- Basic_maths/gcd_hcf.cpp
+++ Basic_maths/gcd_hcf.cpp
@@ -13,6 +13,30 @@ int gcd_hcf(int num_1, int num_2)
     }
     return gcd_hcf;
 }
+
+int gcd_hcf_optimized(int num_1, int num_2)
+{
+    int gcd_hcf = 0;
+    for (int i = min(num_1, num_2); i >= 1; i--)
+    {
+        if (num_1 % i == 0 && num_2 % i == 0)
+        {
+            gcd_hcf = i;
+            break;
+        }
+    }
+    return gcd_hcf;
+}
+int gcd_hcf_best(int num_1, int num_2)
+{
+    while (num_2 > 0)
+    {
+        int rem = num_1 % num_2;
+        num_1 = num_2;
+        num_2 = rem;
+    }
+    return num_1;
+}
 int main()
 {
 
@@ -22,7 +46,9 @@ int main()
     cout << "Enter the first number : ";
     cin >> num_2;
 
-    int answer = gcd_hcf(num_1, num_2);
+    // int answer = gcd_hcf(num_1, num_2);
+    // int answer = gcd_hcf_optimized(num_1, num_2);
+    int answer = gcd_hcf_best(num_1, num_2);
     cout << "GCD(" << num_1 << "," << num_2 << ") : " << answer << endl;
     cout << endl;
 
